be31fb21c34292d7dd64e36e91f88baea0a9b3a4
Julian Rivera <dev@juarri.com>
2024-04-05 13:18:11 -0700

    updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,12 @@ import { defineConfig, sharpImageService } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://lascasasstr.com",
-  integrations: [tailwind(), mdx(), sitemap()],
-  experimental: {
-    assets: true,
-  },
+  integrations: [tailwind(), mdx(), sitemap(), icon()],
   image: {
     service: sharpImageService(),
   },
